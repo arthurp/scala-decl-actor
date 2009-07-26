e5df2053f7fe93f803b572a01668b73d5807111d
Arthur Peters <amp@singingwizard.org>
2009-07-26 16:21:38 -0400

    Create new real tests

--- test/scala/actors/Checks.scala
+++ test/scala/actors/Checks.scala
@@ -7,23 +7,98 @@
 
 package scala.actors
 
-import org.scalacheck._
+//import org.scalacheck.Prop
+import org.scalatest.junit.JUnit3Suite
+import org.scalatest.prop.Checkers
 
-object Checks extends Properties("String") {
-  specify("startsWith", (a: String, b: String) => (a+b).startsWith(a))
+class Checks extends JUnit3Suite with Checkers {
+    var lastS2 : String = _
 
-  specify("endsWith", (a: String, b: String) => (a+b).endsWith(b))
+    class Actor1 extends DeclActor {
+        val sync = new SyncMessage1[String, Int]
+        val sync2 = new SyncMessage2[String, Int, Int]
+        val async = new AsyncMessage1[String]
+        val async2 = new AsyncMessage2[String, String]
+        val done = new SyncMessage0[Unit]
+        val wrongType = new SyncMessage0[Int]
 
-  // Is this really always true?
-  specify("concat", (a: String, b: String) =>
-    (a+b).length > a.length && (a+b).length > b.length
-  )
+        var pre = ""
 
-  specify("substring", (a: String, b: String) =>
-    (a+b).substring(a.length) == b
-  )
+        override def act() = while(true) { receive {
+                case sync(i) => reply( pre + "(" + i + ")" )//; println(pre + "(" + i + ")")
+                case sync2(i, j) => reply( pre + "(" + i*j + ")" )//; println(pre + "(" + i*j + ")")
+                case async(s) => pre = s
+                case async2(s, s2) => pre = s; lastS2 = s2
+                case done() => new RuntimeException().printStackTrace(); reply(()); exit
+                case wrongType() => reply(pre)
+            } }
+    }
 
-  specify("substring", (a: String, b: String, c: String) =>
-    (a+b+c).substring(a.length, a.length+b.length) == b
-  )
+    def testSync1 {
+        val a = new Actor1
+        a.start
+
+        check( (i:Int) => { a.sync(i) == "("+i+")" } )
+        check( (i:Int) => { a.sync !?(i) == "("+i+")" } )
+
+        check( (i:Int) => {
+                val f = a.sync !!(i)
+                f() == "("+i+")"
+            } )
+    }
+
+    def testAsync1 {
+        val a = new Actor1
+        a.start
+        
+        check( (s:String, i:Int) => {
+                a.async !(s)
+                a.sync(i) == s + "("+i+")"
+            } )
+    }
+
+    def testSync2 {
+        val a = new Actor1
+        a.start
+
+        check( (i:Int, j:Int) => { a.sync2(i,j) == "("+i*j+")" } )
+        check( (i:Int, j:Int) => { a.sync2 !?(i,j) == "("+i*j+")" } )
+
+        check( (i:Int, j:Int) => {
+                val f = a.sync2 !!(i,j)
+                f() == "("+i*j+")"
+            } )
+    }
+    def testAsync2 {
+        val a = new Actor1
+        a.start
+
+        check( (s:String, s2:String, i:Int) => {
+                a.async2 ! (s, s2)
+                a.async !(s)
+                a.sync(i) == s + "("+i+")" && lastS2 == s2
+            } )
+    }
+
+    def testReturnUnit {
+        val a = new Actor1
+        a.start
+
+        // This will throw if the bug is present
+        a.done()
+    }
+
+    def testWrongType {
+        val a = new Actor1
+        a.start
+
+        assert( 
+            try {
+                a.wrongType()
+                false
+            } catch {
+                case _ : Error => true
+            }
+        )
+    }
 }
